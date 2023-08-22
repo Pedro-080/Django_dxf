cf3a4eaf4a00bff92b977a3dfc680c06080c11c1
Pedro-080 <pa.nascimento12@gmail.com>
2023-08-21 23:30:21 -0300

    v2_main

--- v2_main.py
+++ v2_main.py
@@ -1,15 +1,15 @@
 # flake8: noqa
 import ezdxf
 
-
-def read_dxf_file(file_path):
-    doc = ezdxf.readfile(file_path)
-    modelspace = doc.modelspace()
-
-    for entity in modelspace:
-        print(f"Entity type: {entity.dxftype()}")
-        print(f"Entity data: {entity.dxfattribs()}")
-        print("-" * 30)
+class Vertice:
+    def __init__(self,num,x,y):
+        self.num = num
+        self.coordx = x
+        self.coordy = y
+        ...
+        
+    def __str__(self):
+        return f"{self.num},{self.coordx:.4f},{self.coordy:.4f}"
 
 
 
@@ -20,39 +20,29 @@ def get_lwpolyline_vertices(file_path,path_write):
     lwpolylines = [entity for entity in modelspace if entity.dxftype() == "LWPOLYLINE"]
     # Filtra todas as entidades no espaço de modelo e pega somente as do tipo "LWPOLYLINE"
 
+    points = [] #Lista vazia que guardará os objetos do tipo Verticev1_main.py
+
     for lwpolyline in lwpolylines:
         print(f"LWPOLYLINE with {len(lwpolyline)} vertices:")
         # Imprime o número de vértices da polilinha leve
-
-        with open(path_write,'w') as file:
-            file.write("")  #Limpa o arquivo
-        
-
+      
 
         for vertex,num in zip(lwpolyline.vertices(),range(len(lwpolyline))):
             x, y = vertex  # Desempacota a tupla do vértice em coordenadas X e Y
-            # write_txt(path_write,num,x,y)
-            
-            with open(path_write,"r") as file:
-                linhas = file.readlines()
-          
-            with open(path_write,"w") as file:
-                for linha in linhas:
-                    file.write(linha)
-                file.write(f"{num+1},{x:.4f},{y:.4f}\n")
-            
+            point = Vertice(num=num+1,x=x,y=y)
+            points.append(point)
+        
+        
+        # print(len(points))
+
+        write_txt(path_write,points)
         print("-" * 30)
 
 
-def write_txt(path_file,num,x,y):
-    with open(path_file,"r") as file:
-        linhas = file.readlines()
-        
+def write_txt(path_file,lista):
     with open(path_file,"w") as file:
-        for linha in linhas:
-            file.write(linha)
-            
-        file.write(f"{num+1},{x:.4f},{y:.4f}\n")
+        for obj in lista:
+            file.write(str(obj) + "\n")    
     ...
     
 
